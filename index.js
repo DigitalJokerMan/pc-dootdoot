0e65d2f8116500f70b822d2083cc16439b5ea05a
Alex K <aiden.hapless@gmail.com>
2021-05-03 21:41:27 -0600

    added mute and unmute sounds

--- index.js
+++ index.js
@@ -11,11 +11,18 @@ function playSound(url, volume) {
 
 class DootDootPlugin extends Plugin {
     async startPlugin() {
-        const SoundPlayer = await getModule([ "playSound" ]);
+        const SoundPlayer = await getModule(["playSound"]);
 
         inject("tp-playSound", SoundPlayer, "playSound", (e) => {
+            console.log(e[0]);
             if (e[0] === "message1") {
                 playSound("https://raw.githubusercontent.com/DigitalJokerMan/pc-dootdoot/master/dootdoot.ogg", e[1]);
+                return false;
+            } else if (e[0] === "unmute") {
+                playSound("https://raw.githubusercontent.com/DigitalJokerMan/pc-dootdoot/master/mute1.ogg", e[1]);
+                return false;
+            } else if (e[0] === "mute") {
+                playSound("https://raw.githubusercontent.com/DigitalJokerMan/pc-dootdoot/master/mute2.ogg", e[1]);
                 return false;
             }
 
