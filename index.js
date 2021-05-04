99ed6d788823bee938d9691c6aa82dee168e94b5
Alex K <aiden.hapless@gmail.com>
2021-05-03 21:49:18 -0600

    implemented deafen sounds

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ class DootDootPlugin extends Plugin {
             } else if (e[0] === "mute") {
                 playSound("https://raw.githubusercontent.com/DigitalJokerMan/pc-dootdoot/master/mute2.ogg", e[1]);
                 return false;
+            } else if (e[0] === "undeafen") {
+                playSound("https://raw.githubusercontent.com/DigitalJokerMan/pc-dootdoot/master/deafen1.ogg", e[1]);
+                return false;
+            } else if (e[0] === "deafen") {
+                playSound("https://raw.githubusercontent.com/DigitalJokerMan/pc-dootdoot/master/deafen2.ogg", e[1]);
+                return false;
             }
 
             return e;
